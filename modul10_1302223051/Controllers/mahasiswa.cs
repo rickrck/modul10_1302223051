e58f762cea4b14ce33ab502b9b6115ae5d8c6482
Ricky <rickyrenaldi100@gmail.com>
2024-04-30 13:28:45 +0700

    menambahkan API Mahasiswa

---
+++ modul10_1302223051/Controllers/mahasiswa.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302223051.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mahasiswa : ControllerBase
+    {
+        public static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Ricky Renaldi","1302223051", new List<string>(){"KPL", "PBO", "BasDat" }, 2022),
+            new Mahasiswa("Nicholas Xavier Robinson Silalahi", "1302220078", new List<string>(){"KPL", "PBO", "BasDat" }, 2022),
+            new Mahasiswa("Syauqi Dhiya Ulhaq", "1302220112", new List<string>(){"KPL", "PBO", "BasDat" }, 2022)
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            dataMahasiswa.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ modul10_1302223051/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace modul10_1302223051
+{
+    public class Mahasiswa
+    {
+        public string Name {  get; set; }
+        public string Nim {  get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+            Name = name;
+            Nim = nim;
+            Course = course;
+            Year = year;
+        }
+    }
+}
